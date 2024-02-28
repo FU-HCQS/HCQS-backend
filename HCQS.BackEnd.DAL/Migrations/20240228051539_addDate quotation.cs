dd03fd4c81db08fd82e432561d7f5296f355c8f8
Hong Quan <hongquan.contact@gmail.com>
2024-02-28 12:20:00 +0700

    add create date

---
+++ HCQS.BackEnd.DAL/Migrations/20240228051539_addDate quotation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HCQS.BackEnd.DAL.Migrations
+{
+    public partial class addDatequotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "Quotations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1045c37d-e6eb-4be7-a5c3-fdca47a1fe21",
+                column: "ConcurrencyStamp",
+                value: "27bab1dd-05db-4370-9846-63a71781a7a2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2f28c722-04c9-41fd-85e4-eaa506acda38",
+                column: "ConcurrencyStamp",
+                value: "be20051f-90dc-422e-b00e-f71a6cc50f1a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5f1c676b-50f6-4b6f-9b7e-f59a0c135c0f",
+                column: "ConcurrencyStamp",
+                value: "d920ead8-65ec-4ac9-89c0-4808a10d5e64");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Quotations");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1045c37d-e6eb-4be7-a5c3-fdca47a1fe21",
+                column: "ConcurrencyStamp",
+                value: "aeb97f91-2658-4962-8562-95b1931131e2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2f28c722-04c9-41fd-85e4-eaa506acda38",
+                column: "ConcurrencyStamp",
+                value: "1fbed398-65b7-45fc-b7f9-136c07929477");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5f1c676b-50f6-4b6f-9b7e-f59a0c135c0f",
+                column: "ConcurrencyStamp",
+                value: "19fef0e6-58be-4b20-ae97-3eef4c253d8c");
+        }
+    }
+}
