f45607322e0c3bf854f0cd291fd14291d27abe8c
Khang Pham <phamkhang12378@gmail.com>
2024-03-02 23:18:50 +0700

    add

--- HCQS.BackEnd.Common/Dto/Request/ConstructionConfigRequest.cs
+++ HCQS.BackEnd.Common/Dto/Request/ConstructionConfigRequest.cs
@@ -4,8 +4,6 @@ namespace HCQS.BackEnd.Common.Dto.Request
 {
     public class ConstructionConfigRequest
     {
-        public Guid Id { get; set; }
-
         public double SandMixingRatio { get; set; }
         public double CementMixingRatio { get; set; }
         public double StoneMixingRatio { get; set; }
